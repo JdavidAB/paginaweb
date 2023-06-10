e14d8dbc4cba379e3aa3f9ad3250ffae05688ae0
David <125768213+JdavidAB@users.noreply.github.com>
2023-06-10 13:59:35 -0500

    Update settings.py

--- paginaweb/settings.py
+++ paginaweb/settings.py
@@ -168,3 +168,7 @@ MESSAGE_TAGS = {
     mensajes_de_error .ERROR: 'danger',
 
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
